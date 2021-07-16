c2b1ef8d55f3ec1cee6f1a649497205bd7f65e2b
sandeepSbudhya <sndps67@gmail.com>
2021-07-16 19:19:28 +0530

    added xss detection on souproute

--- Flask_app/example_script.py
+++
@@ -1,8 +0,0 @@
-class Example_class:
-    def __init__(self, name, fromLoc):
-        self.name = name
-        self.fromLoc = fromLoc
-
-    def function1(self):
-        return "some text returned from function"
-
--- Flask_app/server.py
+++ Flask_app/server.py
@@ -1,22 +1,15 @@
 from flask import Flask
-import example_script as script1
 import soupscript as script2
 
 app = Flask(__name__)
 
 @app.route("/")
 def function1():
-    x = script1.Example_class("sandeep","Jayanagar")
-    y=x.function1()
-    print(y)
-    reply={
-        "name":x.name,
-        "address":x.fromLoc
-    }
-    return reply
+    return "route"
 
 @app.route("/souproute")
 def function2():
     y = script2.Examplesoup()
-    reply = y.printgooglefunc()
-    return reply
+    url = "https://xss-game.appspot.com/level1/frame"
+    reply = y.scan_xss(url)
+    return str(reply)
